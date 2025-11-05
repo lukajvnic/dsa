4f27b4536ba6730bc837f98ae937fbafa5ada1cb
nelimalu <lukaj2501@gmail.com>
2025-11-04 23:11:05 -0500

    traversal problems

---
+++ traversals/narypostorder.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+
+        out = []
+
+        for child in root.children:
+            out += self.postorder(child)
+
+        out += [root.val]
+
+        return out
